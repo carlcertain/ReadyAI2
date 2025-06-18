6a89391bee851fe2feb3bf3d357ec16c9cf0f94e
name <email>
2025-06-18 17:03:05 -0400

    Added full url qualifier to images

--- src/components/sections/Banner.tsx
+++ src/components/sections/Banner.tsx
@@ -2,35 +2,38 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Banner: React.FC = () => {
+
+    const homeUrl = window.location.origin;
+
     return (
         <div className="py-16 bg-gray-50">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="lg:text-center mb-12">
                     <h2 className="text-base text-blue-600 font-semibold tracking-wide uppercase">
                         <img
-                            src="/src/images/banner1.png"
+                            src={homeUrl + "/src/images/banner1.png"}
                         /><img
-                            src="/src/images/banner4.png"
+                            src={homeUrl + "/src/images/banner4.png"}
                         /><img
-                            src="/src/images/banner5.png"
+                            src={homeUrl + "/src/images/banner5.png"}
                         /><img
-                            src="/src/images/banner7.png"
+                            src={homeUrl + "/src/images/banner7.png"}
                         /><img
-                            src="/src/images/banner8.png"
+                            src={homeUrl + "/src/images/banner8.png"}
                         /><img
-                            src="/src/images/banner9.png"
+                            src={homeUrl + "/src/images/banner9.png"}
                         /><img
-                            src="/src/images/banner10.png"
+                            src={homeUrl + "/src/images/banner10.png"}
                         /><img
-                            src="/src/images/banner12.png"
+                            src={homeUrl + "/src/images/banner12.png"}
                         /><img
-                            src="/src/images/banner13.png"
+                            src={homeUrl + "/src/images/banner13.png"}
                         /><img
-                            src="/src/images/banner14.png"
+                            src={homeUrl + "/src/images/banner14.png"}
                         /><img
-                            src="/src/images/banner15.png"
+                            src={homeUrl + "/src/images/banner15.png"}
                         /><img
-                            src="/src/images/banner18.png"
+                            src={homeUrl + "/src/images/banner18.png"}
                         />
                     </h2>
                 </div>
