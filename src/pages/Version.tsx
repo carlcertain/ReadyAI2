2b8e0349c5d7bf4c23722d5455ffa58c314b100d
name <email>
2025-07-18 14:12:29 -0400

    Working versioning page

--- src/pages/Version.tsx
+++ src/pages/Version.tsx
@@ -2,9 +2,18 @@ import React from "react";
 
 const Version = () => {
   return (
-    <div>
-      <h1>Version Information</h1>
-      <p>Current Version: 1.0.0</p>
+    <div className="py-16 bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="lg:text-center">
+          <h2 className="text-base text-accent font-semibold tracking-wide uppercase">Versions</h2>
+        </div>
+
+        <div className="mt-16 text-center">
+          <div className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+            Version 1.0.0
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
