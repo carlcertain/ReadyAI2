7203f0aaae412ef65af1fd770510f3f91f95e770
name <email>
2025-08-20 13:53:34 -0400

    Using env variables for firebase

--- src/middleware/firebase.ts
+++ src/middleware/firebase.ts
@@ -11,13 +11,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAdMkmqeeIApiF-WEUFRl-arU-5Qhufgf8",
-  authDomain: "readyaidev.firebaseapp.com",
-  projectId: "readyaidev",
-  storageBucket: "readyaidev.firebasestorage.app",
-  messagingSenderId: "108710483517",
-  appId: "1:108710483517:web:126d216970486f68dc3c54",
-  measurementId: "G-ZPMN8Z59YM"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
